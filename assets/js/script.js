c3984b109ecee42931c0379d390bdb89736b8e38
gw31415 <git@amas.dev>
2022-09-29 16:15:27 +0900

    Removed commented-out smooth scrolling scripts that did not work

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,17 +76,6 @@
 				offset: "bottom-in-view",
 			},
 		);
-		// -----------------------------
-		// Smmoth Scroll
-		// -----------------------------
-		// $('a[href^="#"]').click(function() {
-		// 	const speed = 500;
-		// 	const href = $(this).attr("href");
-		// 	const target = $(href == "#" || href == "" ? "html" : href);
-		// 	const position = target.offset().top;
-		// 	$("html, body").animate({ scrollTop: position }, speed, "swing");
-		// 	return false;
-		// });
 	});
 
 	$(window).on("load", function() {
